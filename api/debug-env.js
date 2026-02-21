4669744773fb646d5025d4d640b2ff39d794b8c6
begeqin-jpg <begeqin@gmail.com>
2026-02-21 22:33:58 +0700

    Delete api directory

--- api/debug-env.js
+++
@@ -1,8 +0,0 @@
-module.exports = (req, res) => {
-  res.status(200).json({
-    hasClientId: !!process.env.PAYPAL_CLIENT_ID,
-    hasSecret: !!process.env.PAYPAL_CLIENT_SECRET,
-    env: process.env.PAYPAL_ENV || null,
-    brand: process.env.PAYPAL_BRAND_NAME || null
-  });
-};
